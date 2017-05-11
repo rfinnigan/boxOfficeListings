940749094a465c360c979f6df1ab4311f0a4d28f
Ruairidh Finnigan <ruairidhfinnigan@gmail.com>
2017-05-11 16:43:11 +1000

    add some validation for venue model

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,6 @@
+# model for a Venue
 class Venue < ApplicationRecord
+  validates :name, presence: true,
+                   length: { minimum: 5 },
+                   uniqueness: { case_sensitive: false }
 end
