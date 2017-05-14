adbf4f7731657a0318062a0c5ef504c1e7d369bf
Ruairidh Finnigan <ruairidhfinnigan@gmail.com>
2017-05-14 12:28:10 +1000

    add shallow nesting of models

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  resources :venues do
-    resources :rooms
+  shallow do
+    resources :venues do
+      resources :rooms do
+        resources :shows
+      end
+    end
   end
-
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
