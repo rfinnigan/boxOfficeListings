c53bba326b056321b867caf0a1401e4ffb17e8f6
Ruairidh Finnigan <ruairidhfinnigan@gmail.com>
2017-05-16 14:58:10 +1000

    Add validation of roomName

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,7 @@
 class Room < ApplicationRecord
   belongs_to :venue
   has_many :shows
+  validates :roomName, presence: true,
+                       length: { minimum: 2 },
+                       uniqueness: { case_sensitive: false }
 end
