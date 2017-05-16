792b1fe3ec38502bddb64340b1f3345bcb11c025
Ruairidh Finnigan <ruairidhfinnigan@gmail.com>
2017-05-16 11:00:23 +1000

    change new rooms to be added on their own screen no longer added when creating A Venue

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,10 +3,27 @@ class RoomsController < ApplicationController
     @room = Room.find(params[:id])
   end
 
+  def new
+    @venue = Venue.find(params[:venue_id])
+    @room = Room.new
+  end
+
+  def create
+    @venue = Venue.find(params[:venue_id])
+    @room = @venue.rooms.create(room_params)
+    redirect_to venue_path(@venue)
+  end
+
   def destroy
     @venue = Venue.find(params[:venue_id])
     @room = @venue.rooms.find(params[:id])
     @room.destroy
     redirect_to venue_path(@venue)
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:roomName)
   end
 end
