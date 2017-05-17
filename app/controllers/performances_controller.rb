c294b3ee633b5dfb9bc0b20dfdd4c6e54e13de59
Ruairidh Finnigan <ruairidhfinnigan@gmail.com>
2017-05-17 12:03:53 +1000

    Add New Performance Action & show w/ Venues
    Add ability to create a new performance for a show from a Venues page.
    Display performances with shows on a venues page.
    Set the default time formats in config/initializers/time_formats.rb

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -1,2 +1,28 @@
 class PerformancesController < ApplicationController
+  before_action :set_venue_room_and_show, only: [:index, :new, :create]
+
+  def new
+    @performance = Performance.new
+  end
+
+  def create
+    @performance = @show.performances.create(performance_params)
+    redirect_to venue_path(@venue)
+  end
+
+  private
+
+  # method for settting show, venue & room instance variables
+  # from passed room_id param
+  # when using shallow nesting this will only work when called
+  # from index, new and create as only these receive this param
+  def set_venue_room_and_show
+    @show = Show.find(params[:show_id])
+    @room = Room.find(@show[:room_id])
+    @venue = Venue.find(@room[:venue_id])
+  end
+
+  def performance_params
+      params.require(:performance).permit(:date, :time, :duration)
+  end
 end
