2568908bfe8ee7b3c1e9638b4b37562d962779d0
Ruairidh Finnigan <ruairidhfinnigan@gmail.com>
2017-05-24 16:04:03 +1000

    Tidy code per linter suggestions

--- app/controllers/performance_imports_controller.rb
+++ app/controllers/performance_imports_controller.rb
@@ -1,3 +1,4 @@
+# Controller class for handling mass imports of Performances from CSV files
 class PerformanceImportsController < ApplicationController
   require 'csv'
 
@@ -8,12 +9,13 @@ class PerformanceImportsController < ApplicationController
   def create
     @performance_import = PerformanceImport.new(performance_import_params)
     if @performance_import.save
-      redirect_to venues_path, notice: "Imported performances successfully."
+      redirect_to venues_path, notice: 'Imported performances successfully.'
     else
       render :new
     end
   rescue ActionController::ParameterMissing => e
-    new.errors.add(:base, 'No File Selected.' )
+    new.errors.add(:base, 'No File Selected.')
+    Rails.logger.error e.to_s
     render :new
   end
 
--- app/models/performance_import.rb
+++ app/models/performance_import.rb
@@ -3,7 +3,7 @@ class PerformanceImport
   attr_accessor :file
 
   def initialize(attributes = {})
-    #raise 'No File Selected' if attributes.blank?
+    # raise 'No File Selected' if attributes.blank?
     attributes.each { |name, value| send("#{name}=", value) }
   end
 
@@ -16,28 +16,34 @@ class PerformanceImport
       imported_performances.each(&:save!)
       true
     else
-      imported_performances.each_with_index do |performance, index|
-        performance.errors.full_messages.each do |message|
-          errors.add :base, "Row #{index + 2}: #{message}"
-        end
-      end
+      add_errors
       false
     end
   end
 
   def imported_performances
     @imported_performances ||= load_imported_performances
-    #Rails.logger.debug "imported_performances attributes hash: #{@imported_performances.attributes.inspect}"
   end
 
   def load_imported_performances
     performances = []
     CSV.foreach(file.path, headers: true) do |row|
-      performance = Performance.find_by(id: row["id"]) || Performance.new
+      performance = Performance.find_by(id: row['id']) || Performance.new
       performance.attributes = row.to_hash
-      Rails.logger.debug "Performance attributes hash: #{performance.attributes.inspect}"
+      Rails.logger.debug "Performance attributes hash:
+                          #{performance.attributes.inspect}"
       performances.push(performance)
     end
-    return performances
+    performances
+  end
+
+  private
+
+  def add_errors
+    imported_performances.each_with_index do |performance, index|
+      performance.errors.full_messages.each do |message|
+        errors.add :base, "Row #{index + 2}: #{message}"
+      end
+    end
   end
 end
