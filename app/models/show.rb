b141d8a7deffc72af84cf1c30799128be5ff00aa
Ruairidh Finnigan <ruairidhfinnigan@gmail.com>
2017-05-25 12:30:45 +1000

    Add show duration default of 60mins
    Show duration defaults to 60mins if not explicitly set at initialization.

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,4 +5,9 @@ class Show < ApplicationRecord
                     length: { minimum: 2 }
   validates :duration, presence: true
   validates :nationality, length: { maximum: 4 }
+
+  # method to set default show length to 1 hr if not set during initialize
+  after_initialize do |show|
+      show.duration ||= 60
+  end
 end
