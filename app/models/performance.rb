61994db3b0e9b931e60bc1c137688ae0c3f7e37f
Ruairidh Finnigan <ruairidhfinnigan@gmail.com>
2017-05-26 13:41:04 +1000

    Add validation for performance date and time
    Performances can not be scheduled if another performance is already scheduled
    with the same start time and date.

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -2,7 +2,10 @@ class Performance < ApplicationRecord
   belongs_to :show
   belongs_to :room
   validates :date, presence: true
-  validates :time, presence: true
+  validates :time, presence: true,
+                   uniqueness: { scope: %i[date room],
+                                 message: 'another performance is scheduled at
+                                 this date and time in this room' }
   validates :room, presence: true
   validates :show, presence: true
 end
