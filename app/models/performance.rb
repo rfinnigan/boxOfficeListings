8be65e32bf1106e35ff8f12ef138f7d3ec0bd88c
Ruairidh Finnigan <ruairidhfinnigan@gmail.com>
2017-05-26 16:04:52 +1000

    Add custom validation to check for overlapping performances
    Performance now performs a custom validate method which checks that it doesn't
    overlap with other performances in the same room on the same day.

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -2,10 +2,28 @@ class Performance < ApplicationRecord
   belongs_to :show
   belongs_to :room
   validates :date, presence: true
-  validates :time, presence: true,
-                   uniqueness: { scope: %i[date room],
-                                 message: 'another performance is scheduled at
-                                 this date and time in this room' }
+  validates :time, presence: true
   validates :room, presence: true
   validates :show, presence: true
+  validate :cannot_overlap_another_perf
+
+  scope :date, (->(date) { where('(date is ?)', date) })
+
+  scope :exclude_self, (->(id) { where.not(id: id) })
+
+  def end_time
+    time + show.duration * 60
+  end
+
+  def cannot_overlap_another_perf
+    range = Range.new time, end_time
+    room.performances.date(date).exclude_self(id).each do |other_perf|
+      overlap_error if range.overlaps?(other_perf.time..other_perf.end_time)
+    end
+  end
+
+  def overlap_error
+    errors.add(:overlap_error, 'There is already an event scheduled at
+                                this time and date in this room!')
+  end
 end
